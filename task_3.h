26e7e12424654e106123c99cf8250dff0ddd2f9f
m4nuul <nikitakortrlr020@gmail.com>
2025-06-02 11:03:12 +0700

    добавлена возможность установки 2 ос на 1 пк

--- task_3.h
+++ task_3.h
@@ -2,18 +2,23 @@
 #include <algorithm>
 using namespace std;
 
+// Рекурсивная функция для размещения систем на компьютерах
+bool canInstallRecursive(vector<int>& mem, const vector<int>& s, int idx) {
+    if (idx == s.size()) return true; // все системы размещены
+    for (int i = 0; i < mem.size(); ++i) {
+        if (mem[i] >= s[idx]) {
+            mem[i] -= s[idx];
+            if (canInstallRecursive(mem, s, idx + 1)) return true;
+            mem[i] += s[idx]; // backtrack
+        }
+    }
+    return false;
+}
+
 // проверка можно ли инсталлировать все системы на компах
 inline bool canInstallSystems(int m, const vector<int>& mem, int n, const vector<int>& s) {
     vector<int> memCopy = mem;
     vector<int> sCopy = s;
-    sort(memCopy.rbegin(), memCopy.rend());
-    sort(sCopy.rbegin(), sCopy.rend());
-    if (n > m) return false;
-    for (int i = 0; i < n; ++i) {
-        if (sCopy[i] > memCopy[i]) {
-            // не хватает паммяти
-            return false;
-        }
-    }
-    return true;
+    sort(sCopy.rbegin(), sCopy.rend()); // сначала большие системы
+    return canInstallRecursive(memCopy, sCopy, 0);
 }
